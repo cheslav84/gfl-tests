04bfc81e6b883d124f58df5f84c60f479407ecfc
Viacheslav Havryliuk <v.cheslav84@gmail.com>
2023-10-06 20:56:37 +0300

    3. Реалізація методу вирішення рівнянь та його тестування.

--- src/main/java/havryliuk/Main.java
+++ src/main/java/havryliuk/Main.java
@@ -1,4 +1,34 @@
 package havryliuk;
 
 public class Main {
+    private static final double A = 2.7;
+    private static final double B = -0.3;
+    private static final double C = 4.0;
+
+    public static void main(String[] args) {
+        double max = (Math.sqrt(Double.MAX_VALUE)) ;
+        System.out.println(Math.pow(8.0E150, 2));
+
+    }
+
+    public Double solveFunction(double x) {
+        return x < 1.4 ? solveFirst(x) : x == 1.4 ? solveSecond(x) : solveThird(x);
+    }
+
+    private Double solveFirst(double x) {
+        double minPossibleValue = -8.0e150;
+        if (x < minPossibleValue) throw new IllegalArgumentException("The argument is too small to be evaluated.");
+        return A * Math.pow(x, 2) + B * x + C;
+    }
+
+    private Double solveSecond(double x) {
+        return (A / x) + Math.sqrt(Math.pow(x, 2) + 1);
+    }
+
+    private Double solveThird(double x) {
+        double maxPossibleValue = 8.0e150;
+        if (x > maxPossibleValue) throw new IllegalArgumentException("The argument is too large to be evaluated.");
+        return (A + B * x) / Math.sqrt(Math.pow(x, 2) + 1);
+    }
+
 }
--- src/test/java/havryliuk/MainTest.java
+++ src/test/java/havryliuk/MainTest.java
@@ -1,7 +1,66 @@
 package havryliuk;
 
-import static org.testng.Assert.*;
+import org.assertj.core.data.Percentage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+//import static org.testng.Assert.*;
+import static org.assertj.core.api.Assertions.*;
+
 
 public class MainTest {
+    private static final double EPS = 10e-6;
+    private Main solver;
+
+    @BeforeMethod
+    public void setUp() {
+        solver = new Main();
+    }
+
+
+    @Test(dataProvider = "solveFunction")
+    public void testSolveFunction(double x, double y) {
+        assertThat(solver.solveFunction(x)).isCloseTo(y, Percentage.withPercentage(1));
+    }
+
+    @Test
+    public void testSolveFunctionTooSmall() {
+        double minPossibleValue = -8.1e150;
+        assertThatThrownBy(() -> solver.solveFunction(minPossibleValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("The argument is too small to be evaluated.");
+    }
+
+    @Test
+    public void testSolveFunctionTooLarge() {
+        double maxPossibleValue = 8.1e150;
+        assertThatThrownBy(() -> solver.solveFunction(maxPossibleValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("The argument is too large to be evaluated.");
+    }
+
+    @DataProvider(name = "solveFunction")
+    public Object[][] dataForRandomArguments() {
+        return new Object[][] {
+                { -1000000, 2700000300004.0 },
+                { -100, 27034 },
+                { -5, 73 },
+                { 0, 4 },
+                { 5, 0.235339362 },
+                { 100, -0.272986351 },
+                { 1000000, -0.2999973 },
+
+                { -8.0e150, 1.728e+302 },
+                { -1000000.5555559, 2700003300006.8600011 },
+                { 0.01, 3.99727 },
+                { 1.4, 3.649036482 },
+                { 1000000.5555559, -0.2999973 },
+                { 8.0e150, -0.299172257 },
+        };
+    }
+
+
+
 
 }
